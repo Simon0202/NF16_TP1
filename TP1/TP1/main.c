3bbf7d470843339a0d4e363139db4daefa3bb489
Simon LAURENT <simon.laurent@gmx.com>
2016-09-27 14:50:27 +0200

    Another small test + \n

--- TP1/TP1/main.c
+++ TP1/TP1/main.c
@@ -11,6 +11,7 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
-    printf("Testing");
+    printf("Testing\n");
+    printf("Another test\n");
     return 0;
 }
